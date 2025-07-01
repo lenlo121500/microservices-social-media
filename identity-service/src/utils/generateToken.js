afb60d04bb43ebb734b7fedb89f374b7a5b47121
lenlo121500 <raulc8808@gmail.com>
2025-07-01 15:53:21 +0800

    added refreshToken model and generateToken util

---
+++ identity-service/src/utils/generateToken.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import RefreshToken from "../models/refreshToken.model.js";
+
+export const generateTokens = async (user) => {
+  const accessToken = jwt.sign(
+    {
+      userId: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "15m" }
+  );
+
+  const refreshToken = crypto.randomBytes(40).toString("hex");
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 7); // refresh token expires in 7 days
+
+  await RefreshToken.create({
+    token: refreshToken,
+    user: user._id,
+    expiresAt,
+  });
+
+  return { accessToken, refreshToken };
+};
