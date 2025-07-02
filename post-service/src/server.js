f0e1040b53d40054b8be321e8bbabd8f510e103e
lenlo121500 <raulc8808@gmail.com>
2025-07-02 13:13:29 +0800

    set up entry point

---
+++ post-service/src/server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import connectDB from "./config/db.js";
+import postRouter from "./routes/post.route.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import logger from "./utils/logger.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/posts", postRouter);
+
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  await connectDB();
+  logger.info(`Server running on http://localhost:${PORT}`);
+});
+
+process.on("unhandledRejection", (reason, message) => {
+  logger.error(`Unhandled Rejection at: ${message} reason: ${reason}`);
+});
