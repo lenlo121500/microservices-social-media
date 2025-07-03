e5c9f519d4459022aaa459ba7075bab32fd9576c
lenlo121500 <raulc8808@gmail.com>
2025-07-03 16:02:52 +0800

    set up cloudinary with multer

---
+++ media-service/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import logger from "./logger.js";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = (file) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: "auto",
+      },
+      (error, result) => {
+        if (error) {
+          logger.error(`Error in cloudinary upload: ${error.message}`);
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+
+    uploadStream.end(file.buffer);
+  });
+};
+
+export default uploadToCloudinary;
