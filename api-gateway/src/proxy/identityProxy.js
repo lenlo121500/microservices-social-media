b27d9022808ed5afd9f82ff18815bbf099a07c0e
lenlo121500 <raulc8808@gmail.com>
2025-07-07 13:05:14 +0800

    Fix: Manually serialize the body

--- api-gateway/src/proxy/identityProxy.js
+++ api-gateway/src/proxy/identityProxy.js
@@ -6,9 +6,13 @@ import { IDENTITY_SERVICE_URL } from "../config/config.js";
 const identityProxy = proxy(IDENTITY_SERVICE_URL, {
   ...proxyOptions,
   proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    const bodyData = JSON.stringify(srcReq.body || {});
     proxyReqOpts.headers["Content-Type"] = "application/json";
+    proxyReqOpts.headers["Content-Length"] = Buffer.byteLength(bodyData);
     return proxyReqOpts;
   },
+  proxyReqBodyDecorator: (bodyContent) => JSON.stringify(bodyContent),
+
   userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
     logger.info(
       `Response received from Identity Service: ${proxyRes.statusCode}`
